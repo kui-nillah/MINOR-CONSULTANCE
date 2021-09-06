83a631f79c4a151299110831b0c100ec9549ca64
godwin nilla <godwinnillah@gmail.com>
2021-09-06 13:45:40 -0700

    third commit

--- doc/js/main.js
+++ doc/js/main.js
@@ -5,3 +5,22 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true
 });
+
+const clc = document.querySelector(".cancel");
+const arr = document.querySelector(".arr_container");
+const left_container = document.querySelector(".left_container");
+
+arr.addEventListener("click", () => {
+    arr.classList.add("active_arr");
+    if (left_container.classList.contains("off")) {
+        left_container.classList.remove("off");
+        left_container.classList.add("active");
+    }
+});
+clc.addEventListener("click", () => {
+    arr.classList.remove("active_arr");
+    if (left_container.classList.contains("active")) {
+        left_container.classList.remove("active");
+        left_container.classList.add("off");
+    }
+});
