63fa928ffe019a7ec85372c83884b57f45653a91
godwin nilla <godwinnillah@gmail.com>
2021-09-10 09:37:49 -0700

    fourth commit

--- doc/js/main.js
+++ doc/js/main.js
@@ -24,3 +24,79 @@ clc.addEventListener("click", () => {
         left_container.classList.add("off");
     }
 });
+
+const dld = document.querySelector(".cancel_one");
+const abb = document.querySelector(".arr_container_one");
+const left_container_one = document.querySelector(".left_container_one");
+
+abb.addEventListener("click", () => {
+    abb.classList.add("active_arr");
+    if (left_container_one.classList.contains("off")) {
+        left_container_one.classList.remove("off");
+        left_container_one.classList.add("active");
+    }
+});
+dld.addEventListener("click", () => {
+    abb.classList.remove("active_arr");
+    if (left_container_one.classList.contains("active")) {
+        left_container_one.classList.remove("active");
+        left_container_one.classList.add("off");
+    }
+});
+
+const ele = document.querySelector(".cancel_two");
+const acc = document.querySelector(".arr_container_two");
+const left_container_two = document.querySelector(".left_container_two");
+
+acc.addEventListener("click", () => {
+    acc.classList.add("active_arr");
+    if (left_container_two.classList.contains("off")) {
+        left_container_two.classList.remove("off");
+        left_container_two.classList.add("active");
+    }
+});
+ele.addEventListener("click", () => {
+    acc.classList.remove("active_arr");
+    if (left_container_two.classList.contains("active")) {
+        left_container_two.classList.remove("active");
+        left_container_two.classList.add("off");
+    }
+});
+
+const flf = document.querySelector(".cancel_three");
+const aee = document.querySelector(".arr_container_three");
+const left_container_three = document.querySelector(".left_container_three");
+
+aee.addEventListener("click", () => {
+    aee.classList.add("active_arr");
+    if (left_container_three.classList.contains("off")) {
+        left_container_three.classList.remove("off");
+        left_container_three.classList.add("active");
+    }
+});
+flf.addEventListener("click", () => {
+    aee.classList.remove("active_arr");
+    if (left_container_three.classList.contains("active")) {
+        left_container_three.classList.remove("active");
+        left_container_three.classList.add("off");
+    }
+});
+
+const glg = document.querySelector(".cancel_four");
+const aff = document.querySelector(".arr_container_four");
+const left_container_four = document.querySelector(".left_container_four");
+
+aff.addEventListener("click", () => {
+    aff.classList.add("active_arr");
+    if (left_container_four.classList.contains("off")) {
+        left_container_four.classList.remove("off");
+        left_container_four.classList.add("active");
+    }
+});
+glg.addEventListener("click", () => {
+    aff.classList.remove("active_arr");
+    if (left_container_four.classList.contains("active")) {
+        left_container_four.classList.remove("active");
+        left_container_four.classList.add("off");
+    }
+});
